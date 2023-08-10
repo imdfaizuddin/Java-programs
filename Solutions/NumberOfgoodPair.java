82600f33afc616f7ede844f4e3d2c93c33545837
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-10 23:14:11 +0530

    Update NumberOfgoodPair.java

--- Solutions/NumberOfgoodPair.java
+++ Solutions/NumberOfgoodPair.java
@@ -1,3 +1,9 @@
+/*
+Q) Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+*/
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int count=0;
