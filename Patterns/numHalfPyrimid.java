d691485c2fef1de74c893b948485e3a12b3d93c4
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-11 19:11:14 +0530

    Update numHalfPyrimid.java

--- Patterns/numHalfPyrimid.java
+++ Patterns/numHalfPyrimid.java
@@ -1,1 +1,19 @@
+import java.util.*;
+public class numberHalfPyramid {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        do{
+            System.out.print("Enter Lines: ");
+            long line= sc.nextLong();
+            for(long i=1;i<=line;i++){
+                    for(long j=1;j<=i;j++){
+                        System.out.print(j);
+                    }
+                System.out.println();
+            }
+
+
+        }while(true);
+    }
+}
 
