2808af0c35d9b1805c7c4dc1529e8a9bc820fcd8
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-11 19:09:07 +0530

    Update floydTriangle.java

--- Patterns/floydTriangle.java
+++ Patterns/floydTriangle.java
@@ -1,1 +1,22 @@
+import java.util.*;
+public class floydTriangle {
+    public static void main(String[] args) {
+       
+        int exit=1;
+        Scanner sc = new Scanner(System.in);
+       while(exit==1){ 
+        int count=1;
+        System.out.print("Enter a Number: ");
+        int n= sc.nextInt();
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+        System.out.print("enter 1 to continue 0 to exit: ");
+        exit=sc.nextInt();
+    }}
+}
 
