538f4fdf84bdc55eee56411bee9a1410ac3a98bc
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-12 20:56:20 +0530

    Create solidRombus.java

---
+++ Patterns/solidRombus.java
@@ -0,0 +1,20 @@
+/**
+ * solidRombus
+ */
+public class solidRombus {
+
+    public static void main(String[] args) {
+        int n=7;
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
