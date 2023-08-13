3e57dd9db29aca24d853ab7bacc16e93071453da
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-13 20:37:09 +0530

    Update SwapIt.java

--- Solutions/SwapIt.java
+++ Solutions/SwapIt.java
@@ -1,1 +1,13 @@
-
+/*
+Given two integers, write a function to swap them without using a temporary variable.
+*/
+class Solution {
+	public int[] canSort(int a, int b) {
+		// Your code goes 
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		int sol[]={a,b};
+		return sol;
+	}
+}
