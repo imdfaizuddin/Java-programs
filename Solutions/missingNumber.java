8068bdc8150afae271fbb715c59044c04e0d2bd8
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-17 23:49:33 +0530

    Update missingNumber.java

--- Solutions/missingNumber.java
+++ Solutions/missingNumber.java
@@ -1,1 +1,17 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+*/
 
+class Solution {
+	public int missingNumber(int[] nums) {
+		// Your code goes here
+        Arrays.sort(nums);
+        int i;
+        for( i=0;i<nums.length;i++){
+            if(nums[i]!=i){
+                break;
+            }
+        }
+        return i;
+	}
+}
