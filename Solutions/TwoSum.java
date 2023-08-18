3935f960de5af1f23ba564822063aba0c8b684c3
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-18 22:54:25 +0530

    Update TwoSum.java

--- Solutions/TwoSum.java
+++ Solutions/TwoSum.java
@@ -1,1 +1,19 @@
-
+/*
+Two Sum O(n^2)
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[] = new int[2];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int s=nums[i]+nums[j];
+                if(s==target){
+                    ans[k]=i;
+                    ans[k+1]=j;
+                }
+            }
+        }
+        return ans;
+    }
+}
