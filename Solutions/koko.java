e446a7b167d5e3b1e184ebcbc2b38f303288e623
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-23 18:49:07 +0530

    Update koko.java

--- Solutions/koko.java
+++ Solutions/koko.java
@@ -1,1 +1,36 @@
+/*
+Koko eats banana
 
+*/
+class Solution {
+    int maxElement(int[] arr){
+        int max=Integer.MIN_VALUE;
+        for(int i: arr){
+            if(i>max)max=i;
+        }
+        return max;
+    }
+
+    int totTime(int[] arr, int k){
+        int ans=0;
+        for(int i: arr){
+            ans+=Math.ceil((double)i/(double)k);
+        }
+        return ans;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int l=1;
+        int e= maxElement(piles);
+        while(l<=e){
+            int mid= l+(e-l)/2;
+            int k=totTime(piles,mid);
+            if(k>h){
+                l=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+        return l;
+    }
+}
