935424747a85c749c44ce7261307be3463ffdf2e
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-08-27 23:08:34 +0530

    Update missingNum.java

--- Solutions/missingNum.java
+++ Solutions/missingNum.java
@@ -1,1 +1,16 @@
-
+/*
+O(n) solution
+*/
+class Solution {
+	public int findDuplicate(int[] nums) {
+		// Your code goes here
+        int n=nums.length-1;
+        long sum= (n*(n+1))/2;
+        long sumArr=0;
+        for(int i=0;i<nums.length;i++){
+            sumArr+=nums[i];
+        }
+        long ans=sumArr-sum;
+        return (int)ans;
+	}
+}
