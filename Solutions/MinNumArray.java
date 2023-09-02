3b22890d25812b1c06c80de2645c9f690e8c6454
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-09-02 23:52:13 +0530

    Update MinNumArray.java

--- Solutions/MinNumArray.java
+++ Solutions/MinNumArray.java
@@ -3,7 +3,7 @@ Q) Given an array of integers . Find the smallest element in the array.
 */
 class Solution {
 	public int findSmallest(int[] nums) {
-		// Your code goes here
+		
           int smallest= nums[0];
         for(int i=0;i<nums.length;i++){
             if(nums[i]<smallest){
