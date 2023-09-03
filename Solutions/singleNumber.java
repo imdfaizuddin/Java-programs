abcc191215564c5b721ff8ad2395ef33315113af
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-09-03 22:59:49 +0530

    Update singleNumber.java

--- Solutions/singleNumber.java
+++ Solutions/singleNumber.java
@@ -1,1 +1,12 @@
-
+/*
+Single Number - XOR solution!
+*/
+class Solution {
+    public int singleNumber(int[] nums) {
+  	int ans=0;
+		for(int i=0;i<nums.length;i++){
+			ans= ans ^ nums[i];
+		}
+		return ans;
+    }
+}
