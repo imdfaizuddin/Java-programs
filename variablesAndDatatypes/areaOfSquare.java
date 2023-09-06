bc0ac278bc623ae1bcbf771f775ea17ef91f19b3
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-09-06 23:17:56 +0530

    Update areaOfSquare.java

--- variablesAndDatatypes/areaOfSquare.java
+++ variablesAndDatatypes/areaOfSquare.java
@@ -1,1 +1,15 @@
+/*
+Simple Java program to calculate area of a square.
+*/
+import java.util.*;
+public class areaOfSquare {
 
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter side of a square: ");
+            int side= sc.nextInt();
+            int area= side*side;
+        System.out.println("Area of the square: "+area);
+
+    }
+}
