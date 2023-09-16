e8fb5ae2aecd28762586bd11d841bd51f693390f
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-09-16 23:28:24 +0530

    Update DeleteNodeInALinkedList.java

--- Solutions/DeleteNodeInALinkedList.java
+++ Solutions/DeleteNodeInALinkedList.java
@@ -10,6 +10,7 @@ class Solution {
     public void deleteNode(ListNode node) {
         //assigning next node's value to current node
         node.val= node.next.val;
+        //assigning next node's next node adress to current node.next
         node.next= node.next.next;
     }
 }
