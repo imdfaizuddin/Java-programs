6aa4403d417039c6b5571f4a6592ff777bb71569
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-09-24 23:48:45 +0530

    Create NextGreaterElement2.java

---
+++ Solutions/NextGreaterElement2.java
@@ -0,0 +1,41 @@
+/*
+https://leetcode.com/problems/next-greater-element-ii/description/
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number. 
+The second 1's next greater number needs to search circularly, which is also 2.
+
+*/
+
+class Solution {
+    public int[] nextGreaterElements(int[] arr) {
+          int n= arr.length;
+          int ans[] = new int[n];
+        Stack <Integer> stk= new Stack <>();
+        for(int i=2*n-1;i>=0;i--){
+            //removing the smaller elements from the stack
+            while(!stk.isEmpty() && stk.peek() <= arr[i%n]){
+                stk.pop();
+            }
+            // returning the next greater element into the answer array
+            if(i<n){
+            // ckecking if stack is empty 
+                if(stk.isEmpty()){
+                    ans[i]=-1;
+                }else{
+                    ans[i]=stk.peek();
+                }}
+            
+            //pushing the value into stack
+            stk.push(arr[i%n]);
+        }
+        return ans;
+    }
+}
