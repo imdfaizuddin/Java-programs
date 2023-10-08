1a9fe6e15ecbbad69ab4ec1f5083cc0f0fb8d2df
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2023-10-08 23:44:20 +0530

    Update Same_tree.java

--- Solutions/Same_tree.java
+++ Solutions/Same_tree.java
@@ -27,6 +27,7 @@ class TreeNode {
 */
 
 class Solution {
+	//helper function to compare tree values
     boolean help(TreeNode p, TreeNode q){
         if(p == null && q == null) return true;
         if(p == null) return false;
