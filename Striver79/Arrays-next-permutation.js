69eb0d8f32e81426333f8077ea12f812241f4d9a
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2024-02-02 23:55:46 +0530

    Create Arrays-next-permutation.js

---
+++ Striver79/Arrays-next-permutation.js
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/next-permutation/description/
+3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+*/
