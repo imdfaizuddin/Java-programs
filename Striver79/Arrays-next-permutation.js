cd824a1fd488528b8bf17105cb402a9b4e6c92f7
imdfaizuddin <136836553+imdfaizuddin@users.noreply.github.com>
2024-02-02 23:56:09 +0530

    Update Arrays-next-permutation.js

--- Striver79/Arrays-next-permutation.js
+++ Striver79/Arrays-next-permutation.js
@@ -26,3 +26,32 @@ Constraints:
 1 <= nums.length <= 100
 0 <= nums[i] <= 100
 */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    let n = nums.length;
+    let idx = -1;
+    for(let i = n-2; i>=0 ; i--){
+        if(nums[i]< nums[i+1]){
+            idx = i; //breakpoint
+            break;
+        }
+    }
+    //if breakpoint does not exist
+    if(idx == -1){
+        return nums.reverse();
+    }
+
+    //finding next greater element
+    for(let i= n-1 ; i > idx ; i--){
+        if (nums[i] > nums[idx]) {
+            [nums[i], nums[idx]] = [nums[idx], nums[i]]; // swap A[i] and A[ind]
+            break;
+        }
+    }
+    nums.splice(idx + 1, n - idx - 1, ...nums.slice(idx + 1).reverse());
+
+};
