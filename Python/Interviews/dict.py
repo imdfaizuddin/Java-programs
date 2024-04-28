52eb5743aeb852cd1c874eb7f7dbf5c4a9734563
Md Faizuddin <imdfaizuddin@gmail.com>
2024-04-28 23:18:09 +0530

    dictionary lamda  global and local scale of variables

--- Python/Interviews/dict.py
+++ Python/Interviews/dict.py
@@ -1,7 +1,26 @@
-freq = {}
-a = "apple"
-freq[a] = 2
+# # freq = {}
+# # a = "apple"
+# # freq[a] = 2
 
-print(freq)
-print("apple" in freq)
-print(a in freq)
+# # print(freq)
+# # print("apple" in freq)
+# # print(a in freq)
+
+# square = lambda x : x*x
+# print(square(5))
+
+# sum = lambda x, y, z : x + y
+# print(sum(2,3,"x"))
+
+x = 1
+def demoFunc(x):
+    x += 2
+    print(x)
+    def inside():
+        print(x)
+    inside()
+demoFunc(x)
+print(x)
+
+name = "Faiz"
+print(f"my name is {name}")
