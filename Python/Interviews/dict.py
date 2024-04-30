6ff767b06267c766c6232dcaa8bfb7f6bd679cf7
Md Faizuddin <imdfaizuddin@gmail.com>
2024-04-30 23:50:48 +0530

    example of dict()

--- Python/Interviews/dict.py
+++ Python/Interviews/dict.py
@@ -1,26 +1,31 @@
-# # freq = {}
-# # a = "apple"
-# # freq[a] = 2
+# Creating a dictionary from personal info
+tup = dict(first="hi",second="how",third="are", fourth="you")
+print (tup)
 
-# # print(freq)
-# # print("apple" in freq)
-# # print(a in freq)
+# a = "apple"
+# freq = {a: 1, "app": 3,}
+# freq[a] = 2
 
-# square = lambda x : x*x
-# print(square(5))
 
-# sum = lambda x, y, z : x + y
-# print(sum(2,3,"x"))
+# print(freq)
+# print("apple" in freq)
+# print(a in freq)
 
-x = 1
-def demoFunc(x):
-    x += 2
-    print(x)
-    def inside():
-        print(x)
-    inside()
-demoFunc(x)
-print(x)
+# # square = lambda x : x*x
+# # print(square(5))
 
-name = "Faiz"
-print(f"my name is {name}")
+# # sum = lambda x, y, z : x + y
+# # print(sum(2,3,"x"))
+
+# x = 1
+# def demoFunc(x):
+#     x += 2
+#     print(x)
+#     def inside():
+#         print(x)
+#     inside()
+# demoFunc(x)
+# print(x)
+
+# name = "Faiz"
+# print(f"my name is {name}")
