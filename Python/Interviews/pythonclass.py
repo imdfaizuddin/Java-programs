9aed98a5df02f9942f6dcd52a50ebd5415917af7
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-01 23:52:43 +0530

    add Car class and getter function

--- Python/Interviews/pythonclass.py
+++ Python/Interviews/pythonclass.py
@@ -1,3 +1,21 @@
+class Car:
+    def __init__(self,brand,model):
+        self.__brand = brand    # __ in brand makes it private and only accessable in class
+        self.model = model
+    # getter function 
+    def get_brand(self):
+        return self.__brand + "!"
+
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+    
+my_car = Car("Tata", "Nexon")
+print(my_car.full_name())
+print(my_car.get_brand())
+
+
+
+# animal class 
 class Animal:
     def __init__(self,name,age) -> None:
         self.name = name
@@ -6,9 +24,10 @@ class Animal:
     def bark(self):
         return f"{self.name} says Woof!"
 class Dog(Animal):
-    def __inti__(self):
+    def __inti__(self,name,age,breed):
         super().__init__(self)
+        self.breed = breed
 
-my_dog = Dog("Molly", 5)
+# my_dog = Dog("Molly", 5, "indian")
 
-print(my_dog.bark())
+# print(my_dog.bark())
