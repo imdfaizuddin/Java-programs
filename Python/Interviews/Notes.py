79f53e680dbb5ab92cf8bd816d78a88fb2dcb01b
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-03 14:58:48 +0530

    character val & print next line in python

---
+++ Python/Interviews/Notes.py
@@ -0,0 +1,7 @@
+print(ord("A")) #output 65
+print(ord("Z")) #output 90
+print(ord("a")) #output 97
+print(ord("z")) #output 122
+
+print("Hello", "end", 87, True, end="\n") #end="\n" == print in nextline
+print("next line")
