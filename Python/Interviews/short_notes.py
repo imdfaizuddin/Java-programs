26ea3bbc14efac72c57c1bb9a1635921f8d85abb
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-08 21:36:34 +0530

    swapcase , split & join

--- Python/Interviews/short_notes.py
+++ Python/Interviews/short_notes.py
@@ -1,3 +1,15 @@
-str1 = "hello"
+str1 = "Hello"
 x = ''.join(sorted(str1))   
 print(x)    # ehllo
+
+print(str1.swapcase())  #swap the case Hello -> hELLO
+
+'''Split and join'''
+def split_and_join(line):
+    # write your code here
+    a = line.split(" ")      #   ['hello', 'how', 'are', 'you', '?']
+    b = "-".join(a)      # hello-how-are-you-?
+    return b
+
+line = "hello how are you ?"
+print(split_and_join(line))
