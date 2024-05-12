8aa8addd75bf4be2e65eac83a567c31c0e83b6cf
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-12 23:27:09 +0530

    symmetrial strings correction

--- Python/Interviews/Strings/symmetrical_string.py
+++ Python/Interviews/Strings/symmetrical_string.py
@@ -1,13 +1,17 @@
-s = "khokho"
+s = "abcab"
 
 n = len(s)
-mid = (n//2)
+if n%2==0:
+    mid = (n//2)
+else:
+    mid = (n//2) +1
 # print(mid)
-i = 0
 j = mid
+i = 0
 ans = 1
-while j < n:
+while j < n and i < mid:
     if s[i] != s[j]:
+        print(s[i],s[j],i,j)
         ans = 0
         break
     
