bb73e57f292c79e49802f56ac648d2d062aee908
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-12 23:43:41 +0530

    updated comments

--- Python/Interviews/short_notes.py
+++ Python/Interviews/short_notes.py
@@ -1,15 +1,20 @@
-str1 = "Hello"
-x = ''.join(sorted(str1))   
-print(x)    # ehllo
+def sorted_exp():
+    str1 = "Hhello"
+    s = sorted(str1)    #   ['H', 'e', 'h', 'l', 'l', 'o']  (H < h)     
+    x = ''.join(sorted(s))  
+    print(x)    # ehllo
+    print(s)    # ehllo
+    print(str1.swapcase())  #swap the case Hello -> hELLO
 
-print(str1.swapcase())  #swap the case Hello -> hELLO
+sorted_exp()
 
 '''Split and join'''
 def split_and_join(line):
     # write your code here
-    a = line.split(" ")      #   ['hello', 'how', 'are', 'you', '?']
+    a = line.split(";")      #   ['hello', 'how', 'are', 'you', '?']
+    print(a)
     b = "-".join(a)      # hello-how-are-you-?
     return b
 
-line = "hello how are you ?"
-print(split_and_join(line))
+# line = "hello; how; are; you; ?"
+# print(split_and_join(line))
