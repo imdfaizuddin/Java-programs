f078cc6a47429c686be3d488df3063d0b223efaa
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-14 17:47:13 +0530

    replace method in string

--- Python/Interviews/short_notes.py
+++ Python/Interviews/short_notes.py
@@ -1,3 +1,4 @@
+'''String to sorted list (sorted(str))'''
 def sorted_exp():
     str1 = "Hhello"
     s = sorted(str1)    #   ['H', 'e', 'h', 'l', 'l', 'o']  (H < h)     
@@ -18,3 +19,13 @@ def split_and_join(line):
 
 # line = "hello; how; are; you; ?"
 # print(split_and_join(line))
+
+'''Replace char in string'''
+def replace_char():
+    s = "hello"
+    new = ''
+    for i in range(len(s)-1,-1,-1):
+        new = new + s[i]
+    new = new.replace('l','k')   
+    print(new)
+replace_char()
