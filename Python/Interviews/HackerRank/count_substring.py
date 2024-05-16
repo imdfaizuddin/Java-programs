6ed65b66feb051d6d57f604950b2ea8fd4ae9db0
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-16 23:28:49 +0530

    update count_substring

--- Python/Interviews/HackerRank/count_substring.py
+++ Python/Interviews/HackerRank/count_substring.py
@@ -18,3 +18,16 @@ sub_string = "CDC"
 
 x = count_substring(string,sub_string)
 print(x)
+
+def cnt_sub(string,sub_string):
+
+    n = len(string) - len(sub_string)
+    nn = len(sub_string)
+    count = 0
+    for i in range(n):
+        if string[i:i+nn] == sub_string:
+            count +=1
+            
+    return count
+
+print(cnt_sub(string,sub_string))
