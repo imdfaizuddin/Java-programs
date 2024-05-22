54583b1328b43b52a371763f16640261fdff6691
Md Faizuddin <imdfaizuddin@gmail.com>
2024-05-22 23:48:53 +0530

    Find missing and additional values in two lists

--- Python/Interviews/GFG/set.py
+++ Python/Interviews/GFG/set.py
@@ -10,4 +10,14 @@ def common_in_two_lists():
     for i in a:
         if i in b:
             print(i, "is common")
-common_in_two_lists()
+# common_in_two_lists()
+
+'''Find missing and additional values in two lists'''
+def missing_additional_vals():
+    list1 = [1, 2, 3, 4, 5, 6]
+    list2 = [4, 5, 6, 7, 8]
+
+    additional_val = set(list1) - set(list2)
+    missing_val = set(list2) - set(list1)
+    print(additional_val , missing_val)
+missing_additional_vals()
