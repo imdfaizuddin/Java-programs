e69dc729679d9f13e34a021859462239f689a9d0
Md Faizuddin <imdfaizuddin@gmail.com>
2024-06-04 23:12:58 +0530

    anagram check using Counter

--- Python/Interviews/Strings/anagram_check.py
+++ Python/Interviews/Strings/anagram_check.py
@@ -11,3 +11,26 @@ def check_anag(str, list):
             print(item, " is not anagram")
 
 check_anag(str, list1)
+
+
+def chk_anagram(l,s):
+    
+    ans = []
+    b = []
+    for item in l:
+        if sorted(item) == sorted(s):
+            ans.append(f"{item} is anagram of {s}")
+            b.append(True)
+        else:
+            ans.append(f"{item} is not an anagram of {s}")
+            b.append(False)
+    return ans,b
+    
+print(chk_anagram(list1,str))   #returns a tuple with both (ans,b) values
+print(chk_anagram(list1,str)[0])    #get only ans[] from tuple (ans,b)
+print(chk_anagram(list1,str)[1])    #gett only b[] from tuple (ans,b)
+
+s1 = 'listen'
+s2 = 'enlist'
+from collections import Counter
+print(Counter(s1)== Counter(s2))
