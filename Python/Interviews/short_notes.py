0dc0418f474701cd58df73a0f0a8d015ec315be3
Md Faizuddin <imdfaizuddin@gmail.com>
2024-06-06 20:19:39 +0530

    zip function

--- Python/Interviews/short_notes.py
+++ Python/Interviews/short_notes.py
@@ -26,6 +26,21 @@ def replace_char():
     new = ''
     for i in range(len(s)-1,-1,-1):
         new = new + s[i]
-    new = new.replace('l','k')   
+    new = new.replace('l','k')   # okkeh
     print(new)
-replace_char()
+# replace_char()
+
+'''Using Zip function'''
+
+def zip_to_tuple_and_dict():
+    a = ("John", "Charles", "Mike")
+    b = ("Jenny", "Christy", "Monica", "king")
+
+    x = zip(a, b)   #The zip function pairs elements from a and b into a zip object. need to convert to tuple to print
+                    # The zip function iterates over the shortest input iterable. In this case, both a and b have length 3. Thus, zipping takes O(n) time, where nn is the number of elements in the tuples.
+    y = tuple(x)
+    print(y)        #(('John', 'Jenny'), ('Charles', 'Christy'), ('Mike', 'Monica'))
+    print(y[0][1])  # Jenny
+    print(dict(y))  # {'John': 'Jenny', 'Charles': 'Christy', 'Mike': 'Monica'}
+
+zip_to_tuple_and_dict()
