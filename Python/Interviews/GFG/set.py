ea025ef319bb137b900881a16d394295c67d035d
Md Faizuddin <imdfaizuddin@gmail.com>
2024-06-17 23:38:37 +0530

    find common elements in three lists using sets

--- Python/Interviews/GFG/set.py
+++ Python/Interviews/GFG/set.py
@@ -20,4 +20,13 @@ def missing_additional_vals():
     additional_val = set(list1) - set(list2)
     missing_val = set(list2) - set(list1)
     print(additional_val , missing_val)
-missing_additional_vals()
+# missing_additional_vals()
+
+'''Find common elements in three lists using sets'''
+def common_in_three_lists():
+    arr1 = [1, 5, 10, 20, 40, 80, 100]         #{80, 20}
+    arr2 = [6, 7, 20, 80, 100]
+    arr3 = [3, 4, 15, 20, 30, 70, 80, 120]
+
+    print(set(arr1) & set(arr2) & set(arr3))
+common_in_three_lists()
