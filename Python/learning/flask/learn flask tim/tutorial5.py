93e309ab8c385df011cd53750772a9f7ec192fc9
Md Faizuddin <imdfaizuddin@gmail.com>
2024-08-01 23:41:13 +0530

    add logout func

--- Python/learning/flask/learn flask tim/tutorial5.py
+++ Python/learning/flask/learn flask tim/tutorial5.py
@@ -22,5 +22,9 @@ def user():
         return f"<h1>{user}</h1>"
     else:
         return redirect(url_for("login"))
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 if __name__ == "__main__":
     app.run(debug=True)
