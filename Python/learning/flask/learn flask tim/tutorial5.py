0a49d1fb7ed41bc7c863a7343141d6b3df9ec2fb
Md Faizuddin <imdfaizuddin@gmail.com>
2024-08-03 20:06:06 +0530

    permanent session to store login info for desired date

--- Python/learning/flask/learn flask tim/tutorial5.py
+++ Python/learning/flask/learn flask tim/tutorial5.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request, session
-
+from datetime import timedelta
 app = Flask(__name__)
 app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
 @app.route("/")
 def home():
     return render_template("index1.html", content="Testing")
@@ -9,6 +10,7 @@ def home():
 @app.route("/login", methods=["POST","GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
         session["user"] = user
         return redirect(url_for("user"))
